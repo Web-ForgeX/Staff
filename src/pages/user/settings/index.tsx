0592d0ace1a94d1d1c0008f6f8d5472633b071e8
Char <char@charcodes.online>
2025-02-10 02:07:12 +0000

    Added better mobile support

--- src/pages/user/settings/index.tsx
+++ src/pages/user/settings/index.tsx
@@ -2,12 +2,11 @@ import USER_SETTINGS_Account_Info from "./sections/account_info";
 import USER_SETTINGS_Session_Controls from "./sections/session_controls";
 export default function User_Settings() {
   return (
-    <div className="max-w-4xl mx-auto mt-4">
+    <div className="max-w-4xl mx-auto mt-4 px-4 sm:px-6">
       <h1 className="text-3xl font-bold mb-6 text-center sm:text-left">
         Account Settings
       </h1>
       <USER_SETTINGS_Account_Info />
-
       <USER_SETTINGS_Session_Controls />
     </div>
   );
--- src/pages/user/stores/index.tsx
+++ src/pages/user/stores/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 export default function User_Stores() {
   return (
-    <div className="max-w-6xl mx-auto pt-10 pb-10">
+    <div className="max-w-6xl mx-auto pt-10 pb-10 px-4 sm:px-6">
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold mb-6 text-center sm:text-left">
           Your Stores
