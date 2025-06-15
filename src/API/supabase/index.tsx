9a531caa3c59539e5af2034a15fe6d6be44ab972
Char <68965006+C-h-a-r@users.noreply.github.com>
2025-06-15 23:20:37 +0100

    Update index.tsx

--- src/API/supabase/index.tsx
+++ src/API/supabase/index.tsx
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://supabase.forgex.net/",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzQ5OTM4NDAwLCJleHAiOjE5MDc3MDQ4MDB9.Kuc1rb69va9lWRsLiclRLoI-CDrOJSBXFFlaJjJzmEA",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzQ5OTM4NDAwLCJleHAiOjE5MDc3MDQ4MDB9.SGoLkmO87rbeDqRzGWjIVVaNqPkMtlOoZRih586ixAU",
 );
 
 export default supabase;
